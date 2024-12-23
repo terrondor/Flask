1c45655cdee26fadccf45f9434c3b1a58926efb9
Дмитрий <dk.ionov@yande.ru>
2024-12-23 23:59:14 +0300

    Last version

---
+++ models.py
@@ -0,0 +1,19 @@
+from backend.database import db
+from flask_login import UserMixin
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    posts = db.relationship("Post", backref="author", lazy=True)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def __repr__(self):
+        return f"<Post {self.title}>"
